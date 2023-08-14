4753fdd31b01cbd4b9c3ca3e6a0979cb90d74b59
sao-coding <jeter.nice@gmail.com>
2023-08-15 04:43:55 +0800

    修復首頁跳轉問題

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
     const router = useRouter()
 
     useEffect(() => {
-        if (user) {
+        if (user && !loading && !error) {
             router.push("/chat")
         }
     }, [user, loading, error])
